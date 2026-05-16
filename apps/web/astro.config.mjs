164c844cfce4d9dd38b683944bec69f1db645dce
Xavi Fortes <itsfortes@gmail.com>
2026-05-16 18:42:00 +0200

    fix(web): remove broken sitemap integration, fix css import order

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://xavifortes.com",
   integrations: [
     mdx(),
-    sitemap(),
     tailwind({
       applyBaseStyles: false,
     }),
